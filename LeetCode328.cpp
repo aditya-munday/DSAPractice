c0413d8303238e25c00d7a4ab3b431a4391a5d23
Aditya <adityamunday@yahoo.com>
2026-05-22 15:25:54 +0530

    Added files

--- LeetCode328.cpp
+++ LeetCode328.cpp
@@ -1,31 +1,46 @@
-ListNode *temp = head;
-ListNode *odd = new ListNode(-1);
-ListNode *even = new ListNode(-1);
-ListNode *temp1 = odd;
-ListNode *temp2 = even;
-int count = 1;
-while (temp != nullptr) {
-  ListNode *next = temp->next;
-  if (count % 2 == 0) {
-    temp2->next = temp;
-    temp2 = temp2->next;
-  }
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+  ListNode *oddEvenList(ListNode *head) {
+    ListNode *temp = head;
+    ListNode *odd = new ListNode(-1);
+    ListNode *even = new ListNode(-1);
+    ListNode *temp1 = odd;
+    ListNode *temp2 = even;
+    int count = 1;
+    while (temp != nullptr) {
+      ListNode *next = temp->next;
+      if (count % 2 == 0) {
+        temp2->next = temp;
+        temp2 = temp2->next;
+      }
 
-  else {
-    temp1->next = temp;
-    temp1 = temp1->next;
-  }
-  temp->next = nullptr;
-  temp = next;
-  count++;
-}
+      else {
+        temp1->next = temp;
+        temp1 = temp1->next;
+      }
+      temp->next = nullptr;
+      temp = next;
+      count++;
+    }
 
-temp = odd->next;
-while (temp != nullptr) {
-  if (temp->next == nullptr) {
-    temp->next = even->next;
-    break;
+    temp = odd->next;
+    while (temp != nullptr) {
+      if (temp->next == nullptr) {
+        temp->next = even->next;
+        break;
+      }
+      temp = temp->next;
+    }
+    return odd->next;
   }
-  temp = temp->next;
-}
-return odd->next;
+};
